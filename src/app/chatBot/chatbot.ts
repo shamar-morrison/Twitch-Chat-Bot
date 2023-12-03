8343faef7a4cb43fbfcdccecfbc9384b51609f60
Shamar Morrison <shamar.webdev@gmail.com>
2023-12-03 04:15:43 -0500

    Update Twitch API integration

--- src/app/chatBot/chatbot.ts
+++ src/app/chatBot/chatbot.ts
@@ -10,11 +10,13 @@ export class TwitchChatBot {
 
     public twitchClient: any;
     private tokenDetails!: TwitchTokenDetails;
+    private broadcasterId!: string;
 
     constructor(private config: ChatBotConfig) { }
 
     async launch() {
         this.tokenDetails = await this.fetchAccessToken();
+        this.broadcasterId = await this.fetchBroadcasterId();
         this.twitchClient = new this.tmi.Client(
             this.buildConnectionConfig(
                 this.config.twitchChannel,
@@ -45,6 +47,41 @@ export class TwitchChatBot {
             return await TwitchTokenResponseValidator.parseResponse(response.data);
         }).catch(function (error: any) {
             console.log("Failed to get Twitch OAuth Token");
+            console.log(error);
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                throw new TwitchResponseError(error.response.data);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                throw new NoTwitchResponseError(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                throw new MalformedTwitchRequestError(error.request);
+            }
+        })
+    }
+
+    private async fetchBroadcasterId() {
+        const axios = require('axios');
+        console.log("Fetching Broadcaster ID");
+        return axios({
+            method: 'get',
+            url: `https://api.twitch.tv/helix/users`,
+            headers: {
+                'Authorization': `Bearer ${this.tokenDetails.access_token}`,
+                'Client-Id': `${this.config.twitchClientId}`
+            },
+            responseType: 'json'
+        }).then(function (response: any) {
+            // handle success
+            console.log("Successfully fetched Broadcaster ID", response.data.data[0].id);
+            return response.data.data[0].id;
+        }).catch(function (error: any) {
+            console.log("Failed to get Broadcaster ID");
+            console.log(error);
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
@@ -69,14 +106,60 @@ export class TwitchChatBot {
         this.twitchClient.on('message', (channel: any, tags: any, message: any, self: any) => {
             let helloCommand = ['hey', 'hi', 'hello', 'sup', 'yo', 'howdy', 'greetings', 'hola', 'bonjour', 'hallo', 'ciao', 'namaste', 'salaam', 'konnichiwa', 'ni hao', 'shalom', 'jambo', 'merhaba', 'xin chao', 'sawubona', 'privet', 'kamusta', 'sveiki', 'ahoj', 'hujambo', 'kumusta', 'salut', 'kia ora', 'konnichi wa', 'konnichiha'];
 
-            if (helloCommand.includes(message.toLowerCase())) {
+            // check if message includes a hello command and if it was not sent by the bot,
+            // also check if the message is not a command sent by the bot
+            if (helloCommand.includes(message.toLowerCase()) && !self && !message.startsWith('!')) {
                 this.sayHelloToUser(channel, tags);
+            }
+
+            // generate a clip if the message is only the word "!clip" and it was not sent by the bot
+            if (message.toLowerCase() === '!clip' && !self) {
+                this.generateClip(channel, tags).then((clipUrl: string) => {
+                    this.twitchClient.say(channel, `Here's your clip: ${clipUrl}`);
+                }).catch((error: any) => {
+                    console.log(error);
+                    this.twitchClient.say(channel, `Sorry, I couldn't generate a clip for you. Try again later.`);
+                });
             }
         });
     }
 
     private sayHelloToUser(channel: any, tags: any) {
-            this.twitchClient.say(channel, `Hello, ${ tags.username }!`);
+        this.twitchClient.say(channel, `Hello, ${ tags.username }!`);
+    }
+
+    private async generateClip(channel: any, tags: any) {
+         const axios = require('axios');
+        return axios({
+            method: 'post',
+            url: `https://api.twitch.tv/helix/clips?broadcaster_id=${this.broadcasterId}`,
+            headers: {
+                'Authorization': `Bearer ${this.tokenDetails.access_token}`,
+                'Client-Id': `${this.config.twitchClientId}`
+            },
+            responseType: 'json'
+        }).then(function (response: any) {
+            // handle success
+            console.log(response.data.data[0].edit_url);
+            return response.data.data[0].edit_url;
+        }).catch(function (error: any) {
+            
+            console.log(error);
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                throw new TwitchResponseError(error.response.data);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                throw new NoTwitchResponseError(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                throw new MalformedTwitchRequestError(error.request);
+            }
+        })
+    
     }
 
     private buildConnectionConfig(channel: string, username: string, accessToken: string) {
